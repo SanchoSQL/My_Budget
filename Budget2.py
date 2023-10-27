874362a0954b07d52c0eadb0da294e34d034ecaa
Robert Rodriguez <robert54@gmail.com>
2023-10-26 23:07:48 -0500

    Removed old graphs and added test for credit card category

--- Budget2.py
+++ Budget2.py
@@ -59,7 +59,6 @@ cashapp_df = cashapp_df[pd.to_datetime(cashapp_df['Date']).dt.year >= 2023]
 
 #print(cashapp_df.head(3))
 
-
 # Add a new columns 'Source' indicating the source and last updated
 chase_df['Source'] = 'Chase Bank'
 chase_df['Source Updated'] = chase_upd_dt
@@ -72,7 +71,10 @@ cashapp_df['Source Updated'] = cashapp_upd_dt
 df = pd.concat([chase_df, ncu_df, cashapp_df], ignore_index=True)
 
 # Define the categories
-FastFood = 'TST*|TREATS|STARBUCKS|SWIG|KONA ICE|DONUTBOX|DELISH|SMASHBURGER|POPEYES|DOMINO|PANERA|BURGERS|BURGER|TACO|MCDONALD|JERSEY MIKES|PANDA EXPRESS|SONIC|CHICK|BOBA|JAM|THAI|CHIPOTLE|PIZZA|WING|WENDY|RUSSO|ROSAS|BRAUMS|COOKHOUSE|BAHAMA|PIZZERIA|BAGEL|CHICKEN|BISTRO|DAIRY|DD/BR|DOMINO|DRIP N ROLL|MOUNTAIN COO|WHATABURGER|WAFFLE'
+FastFood = 'TREATS|STARBUCKS|SWIG|KONA ICE|DONUTBOX|DELISH|SMASHBURGER|POPEYES|DOMINO|PANERA|BURGERS|BURGER|TACO|MCDONALD|\
+JERSEY MIKES|PANDA EXPRESS|SONIC|CHICK|BOBA|JAM|THAI|CHIPOTLE|PIZZA|WING|WENDY|RUSSO|ROSAS|BRAUMS|COOKHOUSE|BAHAMA|PIZZERIA|\
+BAGEL|CHICKEN|BISTRO|DAIRY|DOMINO|DRIP N ROLL|MOUNTAIN COO|WHATABURGER|WAFFLE|DEBIT TST|TST'
+
 Grocery = 'KROGER|WAL-MART|SPROUTS|H-E-B|HOLLYWOOD FEED|SUPERMARKET|PET SUPPLIES|PETCO'
 Travel = 'NEW ORLEANS|UBER|LYFT|MSY|SAN DIEGO|GUANACASTE'
 Income = 'PAYROLL|DEPOSIT|TAX REF'
@@ -84,7 +86,7 @@ Car = 'TO LOAN 0000'
 Maintenance = 'BRAKES PLUS'
 PersonalCare = 'MCGUINESS|TPD SMILES|LCSW|MASSAGE'
 Bills = '9864031004|CVS CAREPASS|DROPBOX'
-CreditCards = '5920|CAPITAL ONE|CITICTP|CARDMEMBER|TARGET CARD|NEFURNMART'
+CreditCards = '5920|CAPITAL ONE|CITICTP|CARDMEMBER|TARGET CARD|NEFURNMART|DISCOVER|CHASE CREDIT'
 ATM = 'ATM WITHDRAW|ATM FEE'
 
 df['Category'] =np.where(df['Description'].str.contains(Income), 'Income',
@@ -92,18 +94,16 @@ df['Category'] =np.where(df['Description'].str.contains(Income), 'Income',
                 np.where(df['Description'].str.contains(Grocery), 'Grocery',
                 np.where(df['Description'].str.contains(Alcohol), 'Alcohol',
                 np.where(df['Description'].str.contains(Maintenance), 'Maintenance', 
-                np.where(df['Description'].str.contains(FastFood),'Fast Food',        
+                np.where(df['Description'].str.contains(FastFood),'Fast_Food',        
                 np.where(df['Description'].str.contains(Entertainment), 'Entertainment',
-                np.where(df['Description'].str.contains(PersonalCare), 'Personal Care',
+                np.where(df['Description'].str.contains(PersonalCare), 'Personal_Care',
                 np.where(df['Description'].str.contains(Bills), 'Bills',
-                np.where(df['Description'].str.contains(CreditCards), 'Credit Cards',
+                np.where(df['Description'].str.contains(CreditCards), 'Credit_Cards',
                 np.where(df['Description'].str.contains(Travel), 'Travel',
                 np.where(df['Description'].str.contains(Car), 'Car',
                 np.where(df['Description'].str.contains(ATM), 'ATM',
                 np.where(df['Description'].str.contains(Remove), 'Remove', 
                 'Shopping' ))))))))))))))
-print(df.head(3))
-
 
 # create a Subcategory to further break out certain items
 df['Sub-Category'] = df['Description'].apply(lambda x: 'CHICK FIL A' if 'CHICK' in x 
@@ -152,105 +152,42 @@ else 'STARBUCKS' if 'STARBUCKS' in x
 else 'BAHAMA BUCKS' if 'BAHAMA BUCKS' in x
 else 'MISC') 
 
-output = "R:/Dropbox/BillCSV/TEST3.xlsx"
-df.to_excel(output)
-
 df['Date'] = pd.to_datetime(df['Date'])
 df['year_month'] = df['Date'].dt.strftime('%Y-%m')
 df['month_day'] = df['Date'].dt.strftime('%m-%d')
-print(df.head(3))
 
+# This is a test to confirm the fast food category does not contain certain CC sub-categories
 
+df_filtered2test = df[(df['Category'] == 'Fast_Food') & (df['Sub-Category'].isin(['CAPITAL ONE', 'CHASE', 'DISCOVER']))]
+df_filtered2test
 
-Net_Worth_Table = df.groupby('year_month')['Amount'].sum().reset_index(name ='sum')
-Net_Worth_Table['cumulative sum'] = Net_Worth_Table['sum'].cumsum()
-Net_Worth_Chart = go.Figure(
-    data = go.Scatter(x = Net_Worth_Table["year_month"], y = Net_Worth_Table["cumulative sum"]),
-    layout = go.Layout(
-        title = go.layout.Title(text = "Net Worth Over Time")
-    )
-)
-Net_Worth_Chart.update_layout(
-    xaxis_title = "Date",
-    yaxis_title = "Net Worth (£)",
-    hovermode = 'x unified'
-    )
-Net_Worth_Chart.update_xaxes(
-    tickangle = 45)
-Net_Worth_Chart.show()
-
-
-df = df[df.Category != "Income"] 
-df.Amount = df.Amount*(-1)  
-
-output = "R:/Dropbox/BillCSV/TEST2.xlsx"
+output = "R:/Dropbox/BillCSV/TEST3.xlsx"
 df.to_excel(output)
 
-#TESTING :0
+# This will create a df for monthly income
 
-fig = px.sunburst(df, path=['Category'], values='Amount', color='Category')
-fig.show()
+Income_Table = df.query("Amount > 0").groupby('year_month')['Amount'].sum().reset_index(name ='sum')
+Income_Table
 
-Total_Monthly_Expenses_Table = df.groupby('year_month')['Amount'].sum().reset_index(name='sum')
-Total_Monthly_Expenses_Table = Total_Monthly_Expenses_Table.rename(columns={'year_month': 'DATE', 'sum': 'TOTAL EXPENSE'})
-Total_Monthly_Expenses_Table
-Total_Monthly_Expenses_Chart = px.bar(Total_Monthly_Expenses_Table, x="DATE", y="TOTAL EXPENSE",title="Total Monthly Expenses")
-Total_Monthly_Expenses_Chart.update_yaxes(title='Expenses (£)', visible=True, showticklabels=True)
-Total_Monthly_Expenses_Chart.update_xaxes(title='Date',visible=True, showticklabels=True)
+# filter income from df and convert negative amounts 
 
-Total_Monthly_Expenses_Chart.show()
+df = df[df.Category != "Remove"] 
+df['Amount'] = df.Amount*(-1)  
 
-Expenses_Breakdown_Table = pd.pivot_table(df, values = ['Amount'], index = ['Category', 'year_month'], aggfunc=sum).reset_index()
-Expenses_Breakdown_Table.columns = [x.upper() for x in Expenses_Breakdown_Table.columns]
-print(Expenses_Breakdown_Table.head(3))
-Expenses_Breakdown_Chart = px.line(Expenses_Breakdown_Table, x='YEAR_MONTH', y="AMOUNT", title="Expenses Breakdown", color = 'CATEGORY')
-Expenses_Breakdown_Chart.update_yaxes(title='Expenses (£)', visible=True, showticklabels=True)
-Expenses_Breakdown_Chart.update_xaxes(title='Date', visible=True, showticklabels=True) 
-Expenses_Breakdown_Chart.show()
+#categories = df.loc[:, 'Category'].unique()
 
+# create a table for each category
 
-# TEST THIS
-fig = px.pie(df, values='Amount', names='Category')
-fig.show()
+categories = df['Category'].unique()
+dfs = {}
+for category in categories:
+    dfs[f"{category}_Table"] = df[df['Category'] == category].groupby('year_month')['Amount'].sum().reset_index(name=f"{category}_sum")
+for key in dfs:
+    exec(f"{key} = dfs['{key}']")
+    print(key)
 
 
-fig = px.pie(df, values='Amount', names='Sub-Category')
-fig.show()
+%whos DataFrame
+Credit_Cards_Table
 
 
-Expenses_Breakdown_Table = pd.pivot_table(df, values = ['Amount'], index = ['Category', 'year_month'], aggfunc=sum).reset_index()
-Expenses_Breakdown_Table.columns = [x.upper() for x in Expenses_Breakdown_Table.columns]
-Expenses_Breakdown_Table = Expenses_Breakdown_Table.rename(columns={'YEAR_MONTH': 'DATE'})
-Expenses_Breakdown_Table = Expenses_Breakdown_Table[['DATE', 'CATEGORY', 'AMOUNT']]
-
-print(Expenses_Breakdown_Table.iloc[-5:]) # print last row
-
-#Creation of a df with all dates within the range we have data for each category /
-#(e.g. for cash withdrawal there are no transactions between MArch 2020 and August 2020 /
-#so there is not data point in the graph for those dates and I have to fix that)
-
-Expenses_Breakdown_Table_All_Dates = Expenses_Breakdown_Table.set_index(
-    ['DATE', 'CATEGORY']
-).unstack(
-    fill_value=0
-).asfreq(
-    'M', fill_value=0
-).stack().sort_index(level=1).reset_index()
-
-Expenses_Breakdown_Table_All_Dates['DATE'] = pd.to_datetime(Expenses_Breakdown_Table_All_Dates['DATE'])
-Expenses_Breakdown_Table_All_Dates['DATE'] = Expenses_Breakdown_Table_All_Dates['DATE'].dt.strftime('%Y-%m')
-print(Expenses_Breakdown_Table_All_Dates.iloc[-5:]) # print last row
-#Appending all dates to the original df
-
-Expenses_Breakdown_Table_Final = pd.concat([Expenses_Breakdown_Table,Expenses_Breakdown_Table_All_Dates])
-Expenses_Breakdown_Table_Final = Expenses_Breakdown_Table_Final.drop_duplicates(subset = ['DATE', 'CATEGORY'], keep = 'first')
-Expenses_Breakdown_Table_Final = Expenses_Breakdown_Table_Final.sort_values(['DATE', 'CATEGORY'], ascending=[True, False],ignore_index=True)
-Expenses_Breakdown_Table_Final
-#creating a df only for the latest date
-#I need it to add 0s in case in the latest date there was no transaction for a specific category
-#otherwise again the data point for the latest date will not appear in the chart
-#(before I fixed only the dates within the date range of the transactions, not the latest date)
-
-latest_date = Expenses_Breakdown_Table_Final['DATE'].max()
-df_latest_date = Expenses_Breakdown_Table_Final.loc[Expenses_Breakdown_Table_Final['DATE'] == latest_date]
-df_latest_date
